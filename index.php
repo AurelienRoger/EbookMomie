1c7afbad0e006bdb34f3e67d849c9befda426b43
Aurelien Macbook <aurelien.roger1@gmail.com>
2022-12-19 12:11:07 +0100

    Ajout de la fonction de tri des elements par get

--- index.php
+++ index.php
@@ -5,11 +5,28 @@ include 'db/db.php';
 
 $sql = "SELECT * FROM livres";
 
-$pdoStatement = $conn->query($sql) ;
-$listLivre = $pdoStatement->fetchAll();
+
 
 //Pour debug
 //var_dump($listLivre);
+
+if (!empty($_GET['order'])){
+    if ($_GET['order'] == 'nom'){
+        $sql .= ' ORDER BY auteurNom';
+    }
+    elseif ($_GET['order'] == 'prenom'){
+        $sql .= ' ORDER BY auteurPrenom';
+    }
+    elseif ($_GET['order'] == 'titre'){
+        $sql .= ' ORDER BY Titre';
+    }
+    // TODO #1 Faire la recherche pour date aussi 
+}
+
+$pdoStatement = $conn->query($sql) ;
+$listLivre = $pdoStatement->fetchAll();
+
+// TODO #2 Gerer les boutons supprimer et modifier
 
 include 'tpl/index.tpl.php';
 ?>
