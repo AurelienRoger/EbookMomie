d71e504913541889b78a7bd4b2c1d5598adb3c2d
Aurelien Macbook <aurelien.roger1@gmail.com>
2022-12-22 14:31:07 +0100

    affichage de la date en année

--- tpl/index.tpl.php
+++ tpl/index.tpl.php
@@ -62,7 +62,7 @@
                     <td><?= $value['auteurNom'] ?></td>
                     <td><?= $value['Titre'] ?></td>
                     <?php $date = new DateTime($value['AnneeAchat']); ?>
-                    <td><?= $date ->format('d-m-Y')?></td>
+                    <td><?= $date ->format('Y')?></td>
                     <td>
                     <a href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16" data-bs-toggle="modal" data-bs-target="#exampleModal<?=$value['id']?>" id="mod-button">
