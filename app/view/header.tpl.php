ea78a7cd589dad5e00b503d2024afa02f862073d
Aurelien Macbook <aurelien.roger1@gmail.com>
2023-01-01 21:37:27 +0100

    fix liens dans menu

--- app/view/header.tpl.php
+++ app/view/header.tpl.php
@@ -17,14 +17,14 @@
       <!-- TODO #2 a mettre les liens -->
     <nav class="navbar navbar-expand-lg bg-light">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#">Ebook Momie</a>
+              <a class="navbar-brand" href="<?=$_SERVER["BASE_URI"]?>">Ebook Momie</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
               <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
                   <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Acceuil</a>
+                    <a class="nav-link active" aria-current="page" href="<?=$_SERVER["BASE_URI"]?>">Acceuil</a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="ajouter">Ajouter</a>
